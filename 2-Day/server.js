783b02e372c66479d5ad8018c2276f21d6fdf29e
Samarth Varshney <samarthvarshney08022005@gmail.com>
2026-01-23 20:45:22 +0530

    commit changes

--- 2-Day/server.js
+++ 2-Day/server.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const catMe = require('cat-me')
 
 const app = express()
 
 app.get('/',(req,res)=>{
-    res.send("Hi")
-})
+    res.send(catMe())
+})  
 
 app.listen(4000)
